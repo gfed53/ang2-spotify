65e6d30c48e0144ef0bd4fe6bd45cb0020b0e299
gfed53 <gfederico7153@gmail.com>
2017-09-06 12:51:38 -0400

    Added routes for search and authorization, added template and sass for oauth state.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
  
 import { AppComponent }   from './app.component';
+import { SearchComponent } from './search/search.component';
 import { OAuthComponent } from './oauth/oauth.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' }
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'search',  component: SearchComponent },
+  { path: 'get-auth',  component: OAuthComponent }
 ];
 
 @NgModule({
--- src/app/oauth/oauth.component.ts
+++ src/app/oauth/oauth.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'oauth',
-  templateUrl: './oauth.component.html'
+  templateUrl: './oauth.component.html',
+  styleUrls: ['./oauth.component.sass']
 })
 
 export class OAuthComponent {
