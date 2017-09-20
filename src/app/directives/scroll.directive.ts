2545d6032c111d5d572657b02ce1717ec86ec257
gfed53 <gfederico7153@gmail.com>
2017-09-20 12:02:31 -0400

    Added AfterContentInit lifecycle component

--- src/app/directives/scroll.directive.ts
+++ src/app/directives/scroll.directive.ts
@@ -1,13 +1,13 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, AfterViewInit, AfterContentInit } from '@angular/core';
 
 @Directive({
 	selector: '[spScroll]'
 })
-export class ScrollDirective {
+export class ScrollDirective implements AfterContentInit {
 	constructor(private elRef:ElementRef) {}
 
-	ngAfterViewInit() {
-		console.log('after view init fired');
+	ngAfterContentInit() {
+		console.log('after content init fired');
 		this.myScroll();
 	}
 
@@ -16,9 +16,13 @@ export class ScrollDirective {
 		// console.log('this.elRef.nativeElement.scrollHeight',this.elRef.nativeElement.scrollHeight);
 		//Get
 		let distance = this.elRef.nativeElement.getBoundingClientRect().top;
-		console.log('distance of element from top?',distance);
+		console.log('distance of element from top',distance);
+		console.log('window.scrollY:',window.scrollY);
+
 		//Set
 		window.scrollTo(0, distance);
+		// window.scrollBy(0, distance);
+
 		console.log('should be done, window.scrollY:',window.scrollY);
 	}
 }
