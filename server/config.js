57756babf2874b67000e9d068ec86049c6933484
gfed53 <gfederico7153@gmail.com>
2018-05-11 12:21:19 -0400

    Updated config.js.

--- server/config.js
+++ server/config.js
@@ -1,7 +1,23 @@
 exports.PORT = process.env.PORT || 3000;
 
 exports.KEYS = {
-	spotID: 'xxxxx'
+	spotID: 'SPOTIFY-CLIENT-ID'
 };
 
-//
+/*
+
+REDIRECT URIs
+
+If you're building your own version of this app, you will need a Spotify client key (set as the value of the 'spotID' property) which you will get when creating a Spotify app.(https://beta.developer.spotify.com/documentation/web-api/quick-start/)
+
+After doing so, you will need to navigate to your app via the dashboard, go to 'Edit Settings', and add your production redirect URIs where it requests them, and save your changes.
+
+./external/oauth-callback.html
+	dev: http://localhost:3000
+	prod: eg. http://my-app123.herokuapp.com
+
+./src/app/services/get-auth-token.service.ts
+	dev: http://localhost:3000/oauth-callback
+	prod: eg. http://my-app123.herokuapp.com/oauth-callback
+
+*/
